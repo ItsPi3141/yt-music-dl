6739bfe766666734c6a44de8545aefdc18bbcbaa
Pi <piisthebestthingintheuniverse@gmail.com>
2024-04-09 23:35:29 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,122 @@
+import { decipherScript, nTransformScript } from "./utils/sig.js";
+import { parseQueryString } from "./utils/utils.js";
+
+const SOURCE_API_URL =
+	"https://www.youtube-nocookie.com/youtubei/v1/player?prettyPrint=false";
+
+const createFetchBody = (videoId) => {
+	return JSON.stringify({
+		videoId: videoId,
+		context: {
+			client: {
+				clientName: "WEB_EMBEDDED_PLAYER",
+				clientVersion: "2.20210622.10.00",
+			},
+			thirdParty: {
+				embedUrl: `https://www.youtube-nocookie.com/embed/${videoId}`,
+			},
+		},
+		playbackContext: {
+			contentPlaybackContext: {
+				signatureTimestamp: 19821, // from youtube build 45986ce4
+			},
+		},
+	});
+};
+
+/**
+ * Returns an ArrayBuffer of the audio from a given YouTube video ID
+ *
+ * @typedef {Object} DownloadOptions
+ * @property {boolean} saveMetadata - whether to save metadata
+ *
+ * @param {string} id - description of the URL parameter
+ * @param {DownloadOptions} options - description of the options parameter
+ * @return {ArrayBuffer} description of the return value
+ */
+const ytMusicDl = async (id, options) => {
+	// itag 140 is medium quality audio
+	// high quality audio doesn't exist?
+	const targetItag = 140;
+
+	const videoInfo = await fetch(SOURCE_API_URL, {
+		method: "POST",
+		credentials: "omit",
+		body: createFetchBody(id),
+	}).then((res) => res.json());
+
+	if (
+		!(
+			videoInfo.playabilityStatus &&
+			videoInfo.playabilityStatus.status === "OK"
+		)
+	)
+		throw new Error("Video unplayable");
+
+	const downloadInfo = videoInfo?.streamingData?.adaptiveFormats?.find(
+		(f) => f.itag === targetItag
+	);
+
+	let videoProtected = false;
+	if (!downloadInfo?.url) {
+		videoProtected = true;
+	}
+
+	if (videoProtected) {
+		const decipher = (url) => {
+			const args = parseQueryString(url);
+			if (!args.s || !decipherScript) return args.url;
+			const components = new URL(decodeURIComponent(args.url));
+			components.searchParams.set(
+				args.sp ? args.sp : "signature",
+				decipherScript(decodeURIComponent(args.s))
+			);
+			return components.toString();
+		};
+		const ncode = (url) => {
+			const components = new URL(decodeURIComponent(url));
+			const n = components.searchParams.get("n");
+			if (!n || !nTransformScript) return url;
+			components.searchParams.set(
+				"n",
+				nTransformScript(decodeURIComponent(n))
+			);
+			return components.toString();
+		};
+		const url =
+			downloadInfo.url ||
+			downloadInfo.signatureCipher ||
+			downloadInfo.cipher;
+		downloadInfo.url = ncode(decipher(url));
+	}
+
+	const downloadUrl = downloadInfo.url;
+
+	console.log(`${downloadUrl}`);
+
+	const chuckSize = 1024 * 256;
+	const prepareChunks = [];
+	for (
+		let i = 0;
+		i < Math.ceil(Number.parseInt(downloadInfo.contentLength) / chuckSize);
+		i++
+	) {
+		prepareChunks.push(
+			`&range=${i * chuckSize}-${i * chuckSize + chuckSize - 1}`
+		);
+	}
+	const downloadedChunks = prepareChunks.map(
+		async (chunk) =>
+			await fetch(`${downloadUrl}${chunk}`).then(
+				async (res) => await res.arrayBuffer()
+			)
+	);
+	await Promise.all(downloadedChunks);
+	const chunks = [];
+	for (const i in downloadedChunks) {
+		chunks.push(await downloadedChunks[i]);
+	}
+	return await new Blob(chunks).arrayBuffer();
+};
+
+export default ytMusicDl;
---
+++ utils/sig.js
@@ -0,0 +1,35 @@
+// from youtube build 45986ce4
+
+export const decipherScript = (sig) => {
+	const transformations = {
+		swapElementAtPos: (array, position) => {
+			const temp = array[0];
+			array[0] = array[position % array.length];
+			array[position % array.length] = temp;
+		},
+		reverseArray: (array) => {
+			array.reverse();
+		},
+		spliceArray: (array, position) => {
+			array.splice(0, position);
+		},
+	};
+	const transform = (text) => {
+		const a = text.split("");
+		transformations.reverseArray(a, 12);
+		transformations.spliceArray(a, 3);
+		transformations.swapElementAtPos(a, 13);
+		transformations.reverseArray(a, 40);
+		transformations.spliceArray(a, 2);
+		transformations.swapElementAtPos(a, 13);
+		transformations.spliceArray(a, 1);
+		transformations.swapElementAtPos(a, 55);
+		transformations.spliceArray(a, 1);
+		return a.join("");
+	};
+	return transform(sig);
+};
+
+export const nTransformScript = (ncode) => {
+	return "enhanced_except_7ZoBkuX-_w8_" + ncode;
+};
